4ec7bce0bce884e0e050bce6d557fce8e3202860
Ujjwal Singhal <ujjwal.singhal@rentomojo.com>
2020-02-12 23:07:00 +0530

    Update fb.js

--- src/config/fb.js
+++ src/config/fb.js
@@ -3,15 +3,18 @@ import 'firebase/firestore';
 import 'firebase/auth'
 
 // Your web app's Firebase configuration
+
+// Replace firebaseConfig with Your Own configurations
+
 var firebaseConfig = {
-    apiKey: "AIzaSyDjyB7FFVsMGJ3wUF0v4Haw1HWlcdOvPYI",
-    authDomain: "primordial-mile-233801.firebaseapp.com",
-    databaseURL: "https://primordial-mile-233801.firebaseio.com",
-    projectId: "primordial-mile-233801",
-    storageBucket: "primordial-mile-233801.appspot.com",
-    messagingSenderId: "1033102123281",
-    appId: "1:1033102123281:web:36302fa32cbc12ec10acb0",
-    measurementId: "G-6QW3R0KC8T"
+    apiKey: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    authDomain: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    databaseURL: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    projectId: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    storageBucket: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    messagingSenderId: "AAAAAAAAAAAAA",
+    appId: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    measurementId: "A-AAAAAAAAAA"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
